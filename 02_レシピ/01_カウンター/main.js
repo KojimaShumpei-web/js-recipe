e2c4a7f26903ee6e22631876d4c554d2c37d04e8
KojimaShumpei-web <kojishun1021@outlook.jp>
2022-10-08 23:11:04 +0900

    [□と○]を学習しました。

--- 02_レシピ/01_カウンター/main.js
+++ 02_レシピ/01_カウンター/main.js
@@ -1,7 +1,14 @@
 const display = document.getElementById("display")
 const plusButton = document.getElementById("plus-button")
-let count = 0
+const display1 = document.getElementById("display1")
+const minusbutton = document.getElementById("minusButton")
+let count = 30
 plusButton.onclick = function () {
   count += 1
   display.textContent = count
+}
+
+minusbutton.onclick = function () {
+  count -= 1
+  display1.textContent = count
 }
