53f234ffd0f443e61f57307c8e44676aad119f49
KojimaShumpei-web <kojishun1021@outlook.jp>
2022-10-12 13:29:29 +0900

    [Trello (保存機能なし)]を学習しました。

---
+++ 02_レシピ/02_Trello（保存機能あり）/main.js
@@ -0,0 +1,30 @@
+const inputElement = document.getElementById("input-todo")
+const container = document.getElementById("cards-container")
+const addButton = document.getElementById("add-button")
+
+addButton.onclick = function () {
+  const text = inputElement.value
+
+  const card = document.createElement("div")
+  card.className = "card"
+
+  const todo = document.createElement("div")
+  todo.className = "todo"
+  todo.textContent = text
+  card.append(todo)
+
+  const deleteButton = document.createElement("div")
+  deleteButton.className = "delete"
+
+  // 削除ボタン を押したときの処理を登録
+  deleteButton.onclick = function () {
+    // カードを削除する
+    card.remove()
+  }
+
+  // 削除ボタン を card の中に追加する
+  card.append(deleteButton)
+  container.append(card)
+
+  inputElement.value = ""
+}
